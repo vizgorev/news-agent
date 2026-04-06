cf1fe50a9123d76169921295ea15e1546f5defc6
vizgorev <vizgorev@gmail.com>
2026-04-06 17:25:19 +0300

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -8,3 +8,15 @@ def send_to_telegram(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
     requests.post(url, json=payload)
+print(response.text)
+import requests
+import os
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_to_telegram(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
+    response = requests.post(url, json=payload)
+    print(response.text)
